c38259d2ce198559b4f32a53185db87f39dd22db
zhushanshan <zhushanshan@shitou.com>
2018-07-12 19:11:07 +0800

    sharding-jdbc

--- src/main/java/com/zss/config/SwaggerConfig.java
+++ src/main/java/com/zss/config/SwaggerConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
